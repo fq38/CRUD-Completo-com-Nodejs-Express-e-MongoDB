1fa09c807bf6e2290709f5802416bdef5d01e4b0
Felipe Quirino <fq38@live.com>
2025-07-23 17:08:30 -0300

    atualiçao.

--- gabarito/VendaMensal.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const VendaMensalSchema = new mongoose.Schema({
-  mes: Number,
-  valorVendido: Number,
-});
-
-export default mongoose.model("VendaMensal", VendaMensalSchema);
